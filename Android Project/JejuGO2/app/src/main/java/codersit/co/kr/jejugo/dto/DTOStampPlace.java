43550aec96333d1839470fcbe32c17bc1443d043
SeoSangEun <4n_@naver.com>
2017-06-09 17:34:01 +0900

    data규약 및 스탬프받기 부분 작업

--- Android Project/JejuGO2/app/src/main/java/codersit/co/kr/jejugo/dto/DTOStampPlace.java
+++ Android Project/JejuGO2/app/src/main/java/codersit/co/kr/jejugo/dto/DTOStampPlace.java
@@ -6,14 +6,21 @@ package codersit.co.kr.jejugo.dto;
 
 public class DTOStampPlace {
 
+    private int id;
     private String gpsX;
     private String gpsY;
     private String placeName;
 
-    public DTOStampPlace(String gpsX, String gpsY, String placeName) {
+
+    public DTOStampPlace(int id, String gpsX, String gpsY, String placeName) {
+        this.id = id;
         this.gpsX = gpsX;
         this.gpsY = gpsY;
         this.placeName = placeName;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getGpsX() {
